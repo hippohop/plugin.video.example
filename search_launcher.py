0b3610eabd766d6d4482c4411edc0e2e37eb5b61
hippohop <32175902+hippohop@users.noreply.github.com>
2025-05-17 21:22:09 +0200

    Delete search_launcher.py

--- search_launcher.py
+++
@@ -1,9 +0,0 @@
-import xbmc
-import sys
-import urllib.parse
-
-if len(sys.argv) > 1:
-    query = sys.argv[1]
-    encoded_query = urllib.parse.quote_plus(query)
-    url = f'plugin://plugin.video.themoviedb.helper/?action=search_movies&query={encoded_query}'
-    xbmc.executebuiltin(f'ActivateWindow(Videos,"{url}",return)')
