161339c453b937d6ecb87c810efa7c30ec4a693c
hippohop <32175902+hippohop@users.noreply.github.com>
2025-05-17 16:07:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import xbmc
 import xbmcgui
-import os
-import xbmcvfs
+import urllib.parse
 
 def show_search_dialog():
     query = xbmcgui.Dialog().input("Hledat film na TMDb", type=xbmcgui.INPUT_ALPHANUM)
     if query:
         xbmc.log(f"[HROCH CINEMA] Vyhledávání přes TMDb Helper (výsledky): {query}", xbmc.LOGINFO)
-        script_path = os.path.join(xbmcvfs.translatePath("special://home/addons/plugin.video.hrochcinema/"), "search_launcher.py")
-        xbmc.executebuiltin(f'RunScript("{script_path}", "{query}")')
+        encoded_query = urllib.parse.quote_plus(query)
+        url = f'plugin://plugin.video.themoviedb.helper/?action=search_movies&query={encoded_query}'
+        xbmc.executebuiltin(f'RunPlugin("{url}")')
 
 if __name__ == '__main__':
     try:
@@ -17,4 +17,3 @@ if __name__ == '__main__':
     except Exception as e:
         xbmc.log(f"[HROCH CINEMA] CHYBA: {str(e)}", xbmc.LOGERROR)
         xbmcgui.Dialog().notification("CHYBA TMDb", f"{str(e)}", xbmcgui.NOTIFICATION_ERROR)
-
