30a35d1135a07a682a668e6341b6389a2c773792
hippohop <32175902+hippohop@users.noreply.github.com>
2025-05-17 15:48:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 import xbmc
 import xbmcgui
 import os
+import xbmcvfs
 
 def show_search_dialog():
     query = xbmcgui.Dialog().input("Hledat film na TMDb", type=xbmcgui.INPUT_ALPHANUM)
     if query:
         xbmc.log(f"[HROCH CINEMA] Vyhledávání přes TMDb Helper (výsledky): {query}", xbmc.LOGINFO)
-        script_path = os.path.join(xbmc.translatePath("special://home/addons/plugin.video.hrochcinema/"), "search_launcher.py")
+        script_path = os.path.join(xbmcvfs.translatePath("special://home/addons/plugin.video.hrochcinema/"), "search_launcher.py")
         xbmc.executebuiltin(f'RunScript("{script_path}", "{query}")')
 
 if __name__ == '__main__':
@@ -16,3 +17,4 @@ if __name__ == '__main__':
     except Exception as e:
         xbmc.log(f"[HROCH CINEMA] CHYBA: {str(e)}", xbmc.LOGERROR)
         xbmcgui.Dialog().notification("CHYBA TMDb", f"{str(e)}", xbmcgui.NOTIFICATION_ERROR)
+
